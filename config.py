965eaa8865899f1c361ac43e30153e1a77e3a85c
Scott DeVoss <scottdevoss@yahoo.com>
2024-04-10 14:02:32 -0600

    circleci

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+from flask_sqlalchemy import SQLAlchemy
 import os
 
 class Config(object):
--- migrations/env.py
+++ migrations/env.py
@@ -1,3 +1,4 @@
+from flask_sqlalchemy import SQLAlchemy
 import logging
 from logging.config import fileConfig
 
