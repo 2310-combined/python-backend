1e6adcf363ebe6dcfb42dab53c2bb0e16c0db6df
Nathan Lambertson <lambertones@gmail.com>
2024-04-16 09:28:38 -0500

    Edit config for heroku deployment

--- config.py
+++ config.py
@@ -1,20 +1,35 @@
 import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+
+# Configuration Classes
 class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # Default to using a PostgreSQL database named 'capstone' on localhost
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'postgresql://localhost/capstone')
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    # Look for DEV_DATABASE_URI environment variable; otherwise, use the default URI
     SQLALCHEMY_DATABASE_URI = os.getenv('DEV_DATABASE_URI', 'postgresql://localhost/capstone_dev')
 
 class TestConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URI', 'postgresql://localhost/capstone_test')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProductionConfig(Config):
-    # In production, DATABASE_URI must be set; there is no default.
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('PROD_DATABASE_URI')
+
+# Environment-specific configuration
+env_config = {
+    "development": DevelopmentConfig,
+    "testing": TestConfig,
+    "production": ProductionConfig
+}
+
+# Set the configuration from environment
+config_type = os.getenv('FLASK_ENV', 'development')
+app.config.from_object(env_config[config_type])
+
+# Initialize SQLAlchemy
+db = SQLAlchemy(app)
