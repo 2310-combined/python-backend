c9be4842197264a8b9b59613f225c2cb70e64e0f
Nathan Lambertson <lambertones@gmail.com>
2024-04-16 09:37:12 -0500

    Edit test config for heroku

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,14 +8,28 @@ from config import TestConfig  # Make sure this import statement is correct
 
 @pytest.fixture(scope='session')
 def app():
-    """Instance of Main flask app configured for testing."""
-    app = create_app(TestConfig)  # Pass the TestConfig class itself
-    with app.app_context():
-        yield app
+    """
+    Create and configure a new app instance for testing.
+    Use the `TestConfig` to configure the app.
+    """
+    app = create_app(TestConfig)
+    # Push an application context to bind the SQLAlchemy object to your app
+    app_context = app.app_context()
+    app_context.push()
+    
+    db.create_all()  # Create all database tables
+
+    yield app  # This will be used by the pytest for running the tests
+    
+    db.session.remove()  # Remove the session
+    db.drop_all()  # Drop all database tables
+    app_context.pop()  # Pop the application context
 
 @pytest.fixture(scope='session')
 def client(app):
-    """Setup Flask test client. This gets executed for each test function."""
+    """
+    Setup Flask test client. This gets executed for each test function.
+    """
     return app.test_client()
 
 @pytest.fixture(scope='session')
