d7eaa1be89539f0556b6cebbf60f214012ecea95
Nathan Lambertson <lambertones@gmail.com>
2024-04-16 11:15:56 -0500

    Edit Config for heroku

--- config.py
+++ config.py
@@ -7,18 +7,22 @@ app = Flask(__name__)
 # Configuration Classes
 class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Default to a local database if no environment-specific URI is provided
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'postgresql://localhost/capstone')
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    # Use a specific development environment database URI or default to a local database
     SQLALCHEMY_DATABASE_URI = os.getenv('DEV_DATABASE_URI', 'postgresql://localhost/capstone_dev')
 
 class TestConfig(Config):
     TESTING = True
+    # Use a specific testing environment database URI or default to a local test database
     SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URI', 'postgresql://localhost/capstone_test')
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.getenv('PROD_DATABASE_URI')
+    # Use the standard Heroku environment variable or another if explicitly set
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').replace("postgres://", "postgresql://", 1) if os.getenv('DATABASE_URL') else 'postgresql://localhost/capstone'
 
 # Environment-specific configuration
 env_config = {
